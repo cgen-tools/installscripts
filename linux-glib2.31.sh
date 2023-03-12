93232f3194a5556c8dee5a155dd96baf7ee51a1f
Luna <imlvnaa@gmail.com>
2023-03-11 21:33:25 -0500

    Update linux-glib2.31.sh

--- linux-glib2.31.sh
+++ linux-glib2.31.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-HOMEDIR=""
+HOMEDIR=`eval echo ~`
+#HOMEDIR=""
 
-if [ -z "$1" ] then
-  HOMEDIR="$1"
-else
-  HOMEDIR=`eval echo ~`
-fi
+#if [ -z "$1" ]; then
+#  HOMEDIR="$1"
+#else
+#  HOMEDIR=`eval echo ~`
+#fi
 
 
 echo Assuming homedir is $HOMEDIR
-
-
+echo
+echo
 echo Making sure unzip is installed
 sudo apt install -y unzip
-
+echo
+echo
+echo
 cd $HOMEDIR
 echo Downloading the game
-
-
+echo
+echo
 wget https://nightly.link/Thlumyn/clangen/workflows/build/development/Clangen_Linux64_glibc2.31%2B.tar.xz.zip
+echo
+echo
+echo
 echo Unzipping...
 unzip Clangen_Linux64_glibc2.31+.tar.xz.zip
 echo Unzipping a second time...
